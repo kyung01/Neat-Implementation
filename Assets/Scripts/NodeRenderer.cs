e800905161e065760adafb5016f7428072dbd225
BlueBug <iopzxz@gmail.com>
2019-10-10 08:47:10 -0700

    Making advanced renderer

---
+++ Assets/Scripts/NodeRenderer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeRenderer : MonoBehaviour
+{
+	[SerializeField]
+	KLineRenderer lineRenderer;
+	public delegate NodeRenderer DelGetNodeRenderer(int index);
+
+	DNANode node;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	/// <summary>
+	/// resets all the connections
+	/// </summary>
+	public void reset()
+	{
+
+	}
+	public void render(DNANode node, DelGetNodeRenderer search)
+	{
+			
+	}
+}
--- Assets/Scripts/OrganismRenderer.cs
+++ Assets/Scripts/OrganismRenderer.cs
@@ -7,14 +7,16 @@ public class OrganismRenderer : MonoBehaviour
 	[SerializeField]
 	SpriteRenderer PREFAB_NODE;
 	[SerializeField]
+	NodeRenderer PREFAB_NODE_RENDERER;
+	[SerializeField]
 	KLineRenderer PREFAB_LINE_RENDERER;
 	// Start is called before the first frame update
 
+	List<DNANode> allNodes = new List<DNANode>();
 	List<SpriteRenderer> inputNodes = new List<SpriteRenderer>();
+	List<SpriteRenderer> hiddenNodes = new List<SpriteRenderer>();
 	List<SpriteRenderer> outputNodes = new List<SpriteRenderer>();
-
-	GameObject outputNodeGroups = null;
-
+	
 	void Start()
     {
         
@@ -25,43 +27,49 @@ public class OrganismRenderer : MonoBehaviour
     {
         
     }
+	SpriteRenderer hprGetNode(float x, float y, float z)
+	{
+		var node = Instantiate(PREFAB_NODE).GetComponent<SpriteRenderer>();
+		node.transform.parent = this.transform;
+		node.transform.localPosition = new Vector3(x,y,z);
+		return node;
+	}
 	public void render(Organism organism)
 	{
+		allNodes.Clear();
+		for (int i = 0; i < organism.HiddenNodeCount; i++)
+		{
+			int x = i % 10;
+			int y = (int)(i /10);
+			var hiddenNode = hprGetNode(4 + x, y, 0);
+			hiddenNode.color = Color.green;
+			hiddenNodes.Add(hiddenNode);
+		}
 		if(inputNodes.Count == 0)
 		{
-			var initialNode = Instantiate(PREFAB_NODE).GetComponent<SpriteRenderer>();
-			initialNode.transform.parent = this.transform;
-			initialNode.transform.localPosition = new Vector3(0, 3, 0);
+			var initialNode = hprGetNode(0, 3, 0);
 			inputNodes.Add(initialNode);
 			for (int i = 0; i < 2; i++)
 			{
 				for (int y = 0; y < 3; y++)
 					for (int x = 0; x < 3; x++)
 					{
-						var node = Instantiate(PREFAB_NODE).GetComponent<SpriteRenderer>();
-						node.transform.parent = this.transform;
-						node.transform.localPosition = new Vector3(3 * i + x, y, 0);
-						inputNodes.Add(node);
+						inputNodes.Add(hprGetNode(3 * i + x, y, 0));
 					}
 			}
 
 		}
-		if(outputNodeGroups == null)
+		if(outputNodes.Count== 0)
 		{
-			outputNodeGroups = new GameObject();
-			outputNodeGroups.transform.parent = this.transform;
-			outputNodeGroups.transform.localPosition = new Vector3(15, 0, 0);
 			for (int y = 0; y < 3; y++)
 			{
 				for (int x = 0; x < 3; x++)
 				{
-					var node = Instantiate(PREFAB_NODE).GetComponent<SpriteRenderer>();
-					node.transform.parent = outputNodeGroups.transform;
-					node.transform.localPosition = new Vector3(x, y, 0);
-					outputNodes.Add(node);
+					outputNodes.Add(hprGetNode(15 + x, y, 0));
 				}
 			}
 				
 		}
+		
 	}
 }
